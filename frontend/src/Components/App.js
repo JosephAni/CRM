51bcf4dcb0ba35c12e5f63151cb67656e63bfcaa
Joseph <joeoani@gmail.com>
2023-10-03 18:53:31 -0400

    frontend

---
+++ frontend/src/Components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import PlayerList from '../Components/Player/PlayerList';
+import PlayerSingle from '../Components/Player/PlayerSingle';
+import PlayerForm from '../Components/Player/PlayerForm';
+
+function App() {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col s12">Menu</div>
+      </div>
+      <div className="row">
+        <div className="col s3"><PlayerList /></div>
+        <div className="col s9"><PlayerSingle /></div>
+      </div>
+      <div className="row">
+        <div className="col s12"><PlayerForm /></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
